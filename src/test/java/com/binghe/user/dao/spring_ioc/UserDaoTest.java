0b59869212fd7c598c7742cd1c280e305221f3c4
binghe819 <binghe819@naver.com>
2021-04-26 18:02:18 +0900

    test: 스프링 IoC는 싱글톤으로 동작한다는 테스트 코드 추가

--- src/test/java/com/binghe/user/dao/spring_ioc/UserDaoTest.java
+++ src/test/java/com/binghe/user/dao/spring_ioc/UserDaoTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import com.binghe.user.domain.User;
 import java.sql.SQLException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -13,11 +14,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 class UserDaoTest {
 
     private UserDao userDao;
+    private ApplicationContext context;
 
     @BeforeEach
     void setUp() throws SQLException, ClassNotFoundException {
         // SpringIoC 컨테이너 (빈 팩토리)를 생성한다. 생성하면서 `DaoFactory`라는 설정 정보를 이용하여 빈을 등록한다.
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        context = new AnnotationConfigApplicationContext(DaoFactory.class);
         userDao = context.getBean("userDao", UserDao.class);
         userDao.deleteAll();
     }
@@ -38,5 +40,16 @@ class UserDaoTest {
             () -> assertThat(foundUser.getName()).isEqualTo(user.getName()),
             () -> assertThat(foundUser.getPassword()).isEqualTo(user.getPassword())
         );
+    }
+
+    @DisplayName("스프링 IoC 컨테이너의 빈 싱글턴 테스트")
+    @Test
+    void singleton() {
+        // given, when
+        UserDao userDao1 = context.getBean("userDao", UserDao.class);
+        UserDao userDao2 = context.getBean("userDao", UserDao.class);
+
+        // then
+        assertThat(userDao1).isSameAs(userDao2);
     }
 }
