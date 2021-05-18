85915c5f3ad74a3af1f95e3ae789d1dfd6570452
binghe819 <binghe819@naver.com>
2021-05-18 17:46:13 +0900

    refactor: 여섯번째 리팩토링. 템플릿/콜백 패턴

---
+++ src/main/java/com/binghe/dao/template_callback/StatementStrategy.java
@@ -0,0 +1,9 @@
+package com.binghe.dao.template_callback;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public interface StatementStrategy {
+    PreparedStatement makePreparedStatement(Connection conn) throws SQLException;
+}
