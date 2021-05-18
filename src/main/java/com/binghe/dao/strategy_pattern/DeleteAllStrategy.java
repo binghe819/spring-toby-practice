dbd823a335327a9a1607ca8d3fd84f231faeb8c5
binghe819 <binghe819@naver.com>
2021-05-18 15:25:37 +0900

    refactor: 세번째 리팩토링. 전략 패턴

---
+++ src/main/java/com/binghe/dao/strategy_pattern/DeleteAllStrategy.java
@@ -0,0 +1,14 @@
+package com.binghe.dao.strategy_pattern;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DeleteAllStrategy implements StatementStrategy {
+
+    @Override
+    public PreparedStatement makePreparedStatement(Connection conn) throws SQLException {
+        PreparedStatement ps = conn.prepareStatement("delete from users");
+        return ps;
+    }
+}
