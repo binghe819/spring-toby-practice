19a4c520644e453643f49aae60631a8212c68c03
binghe819 <binghe819@naver.com>
2021-06-03 11:14:34 +0900

    feat: JavaMail로 사용자에게 업그레이드 통보 기능 구현

--- src/main/java/com/binghe/service/UserService.java
+++ src/main/java/com/binghe/service/UserService.java
@@ -3,9 +3,19 @@ package com.binghe.service;
 import com.binghe.dao.UserDao;
 import com.binghe.domain.Level;
 import com.binghe.domain.User;
+import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.Message.RecipientType;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.sql.DataSource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DataSourceUtils;
@@ -55,6 +65,7 @@ public class UserService {
     protected void upgradeLevel(User user) {
         user.upgradeLevel();
         userDao.update(user);
+        sendUpgradeEmail(user);
     }
 
     public void add(User user) {
@@ -62,5 +73,26 @@ public class UserService {
             user.setLevel(Level.BASIC);
         }
         userDao.add(user);
+    }
+
+    private void sendUpgradeEmail(User user) {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "mail.ksug.org");
+        Session s = Session.getInstance(props, null);
+
+        MimeMessage message = new MimeMessage(s);
+        try {
+            message.setFrom(new InternetAddress("useradmin@ksug.org"));
+            message.addRecipients(RecipientType.TO,
+                String.valueOf(new InternetAddress(user.getEmail())));
+            message.setSubject("Upgrade 안내");
+            message.setText("사용자의 등급이 : " + user.getLevel().name() + "로 업그레이드되었습니다.");
+
+            Transport.send(message);
+        } catch (AddressException e) {
+            throw new RuntimeException(e);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
