afb49829772e638cdd00c2f6b07769d61f8b6b2c
binghe819 <binghe819@naver.com>
2021-06-10 18:15:18 +0900

    test: 비즈니스 로직과 트랜잭션 로직 통합 테스트 (예외 발생시키는 TestUserService이용)

--- src/test/java/com/binghe/service/UserServiceTest.java
+++ src/test/java/com/binghe/service/UserServiceTest.java
@@ -7,8 +7,6 @@ import com.binghe.TestAppConfiguration;
 import com.binghe.dao.UserDao;
 import com.binghe.domain.Level;
 import com.binghe.domain.User;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,23 +14,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.MethodMode;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = TestAppConfiguration.class)
-public class MockUserServiceTest {
+public class UserServiceTest {
 
     @Autowired
-    private UserService userService; // UserServiceImpl(비즈니스 로직)을 가지고 있는 UserService(트랜잭션 경계 설정)
-    @Autowired
-    private UserServiceImpl userServiceImpl;
+    private UserService userService; // UserServiceImpl(비즈니스 로직)을 가지고 있는 UserService(트랜잭션 경계 설정) - add 테스트용
     @Autowired
     private UserDao userDao;
     @Autowired
@@ -98,5 +90,52 @@ public class MockUserServiceTest {
 
         assertThat(userWithLevelRead.getLevel()).isEqualTo(userWithLevel.getLevel());
         assertThat(userWithoutLevelRead.getLevel()).isEqualTo(Level.BASIC);
+    }
+
+    @DisplayName("트랜잭션 테스트 - 비즈니스 로직과 트랜잭션 로직 통합 테스트")
+    @Test
+    void upgradeAllOrNothing() {
+        TestUserService testUserService = new TestUserService(userDao, mailSender, users.get(3).getId());
+
+        UserServiceTx userServiceTx = new UserServiceTx(testUserService, transactionManager);
+
+        userDao.deleteAll();
+        for(User user : users) userDao.add(user);
+
+        try {
+            assertThatThrownBy(() -> userServiceTx.upgradeLevels())
+                .isInstanceOf(TestUserServiceException.class);
+        }
+        catch(TestUserServiceException e) {
+        }
+
+        checkLevelUpgraded(users.get(1), false);
+    }
+
+    private void checkLevelUpgraded(User user, boolean upgraded) {
+        User userUpdate = userDao.get(user.getId());
+        if (upgraded) {
+            assertThat(userUpdate.getLevel()).isEqualTo(user.getLevel().nextLevel());
+        }
+        else {
+            assertThat(userUpdate.getLevel()).isEqualTo(user.getLevel());
+        }
+    }
+
+    static class TestUserService extends UserServiceImpl {
+        private String id;
+
+        public TestUserService(UserDao userDao, MailSender mailSender, String id) {
+            super(userDao, mailSender);
+            this.id = id;
+        }
+
+        protected void upgradeLevel(User user) {
+            if (user.getId().equals(this.id)) throw new TestUserServiceException();
+            super.upgradeLevel(user);
+        }
+    }
+
+    static class TestUserServiceException extends RuntimeException {
     }
 }
