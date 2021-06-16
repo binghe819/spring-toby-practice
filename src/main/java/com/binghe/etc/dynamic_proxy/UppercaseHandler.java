edfeb698acb04dd80bf164b955b9bc84bf5b90ae
binghe819 <binghe819@naver.com>
2021-06-16 13:57:37 +0900

    refactor: 다이내믹 프록시 학습테스트 InvocationHandler 구현체(Uppercase) 유연하게 리팩토링

--- src/main/java/com/binghe/etc/dynamic_proxy/UppercaseHandler.java
+++ src/main/java/com/binghe/etc/dynamic_proxy/UppercaseHandler.java
@@ -6,16 +6,18 @@ import java.lang.reflect.Method;
 import java.util.Locale;
 
 public class UppercaseHandler implements InvocationHandler {
-    private final Hello target;
+    private final Object target; // String에서 Object로 수정
 
-    // 다이내믹 프록시로부터 전달받은 요청을 다시 타깃 객체에 위임해야 하기 때문에 타깃 객체를 주입받는다.
-    public UppercaseHandler(Hello target) {
+    public UppercaseHandler(Object target) {
         this.target = target;
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        String ret = (String) method.invoke(target, args); // 타깃으로 위임(실행). 인터페이스의 메서드 호출에 모두 적용된다.
-        return ret.toUpperCase(); // 부가기능 제공 (리턴값은 클라이언트에게 전해진다.)
+        Object ret = method.invoke(target, args); // 타깃으로 위임(실행).
+        if (ret instanceof String && method.getName().startsWith("say")) {
+            return ((String) ret).toUpperCase();
+        }
+        return ret;
     }
 }
